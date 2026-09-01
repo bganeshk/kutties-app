d00106d4ceb20c3aae9739420e585813a87adf32
ganesba1 <ganesba1@in.ibm.com>
2026-09-01 21:01:15 +0530

    parent notes has been added

---
+++ src/components/parentnote/index.ts
@@ -0,0 +1,3 @@
+export { default as ParentNoteList } from './ParentNoteList';
+export { default as ParentNoteForm } from './ParentNoteForm';
+export { default as ParentNoteRow }  from './ParentNoteRow';
